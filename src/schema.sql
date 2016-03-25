1e4a51ed498cc9c9f6e392c4b2252683f1bfca0e
Caleb Leung <dvcs@calebleung.com>
2016-03-25 13:03:05 -0500

    Schema is in run_once

--- src/schema.sql
+++
@@ -1,7 +0,0 @@
-drop table if exists shortlinks;
-create table shortlinks (
-  id integer primary key autoincrement,
-  link_id text not null,
-  link_url text not null,
-  link_hits integer not null
-);
